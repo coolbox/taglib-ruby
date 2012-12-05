7d7109979f4eee79c06fc555edd65fc6dba869d1
Peter Roome <pete@wearepandr.com>
2012-12-05 12:55:44 +0000

    Increment build

--- ext/extconf_common.rb
+++ ext/extconf_common.rb
@@ -1,6 +1,6 @@
 # Default opt dirs to help mkmf find taglib
 # configure_args = "--with-opt-dir=/usr/local:/opt/local:/sw "
-configure_args = "--with-opt-dir=/usr/local:/opt/local:/sw:#{File.join(File.dirname(__FILE__), 'vendor', 'taglib')} "
+configure_args = "--with-opt-dir=/usr/local:/opt/local:/sw:#{File.join(File.dirname(__FILE__), '../vendor', 'taglib')} "
 ENV['CONFIGURE_ARGS'] = configure_args + ENV.fetch('CONFIGURE_ARGS', "")
 
 require 'mkmf'

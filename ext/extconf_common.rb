79089e1a80193defb84ddb8f7019631d5e7408d3
Peter Roome <pete@wearepandr.com>
2012-12-05 13:54:48 +0000

    f statement to determine lib to use

--- ext/extconf_common.rb
+++ ext/extconf_common.rb
@@ -1,6 +1,10 @@
 # Default opt dirs to help mkmf find taglib
-# configure_args = "--with-opt-dir=/usr/local:/opt/local:/sw "
-configure_args = "--with-opt-dir=/usr/local:/opt/local:/sw:#{File.join(File.dirname(__FILE__), '../vendor', 'taglib')} "
+if `uname -a`.strip[/Linux/i] && `uname -a`.strip[/x86_64/i]
+  configure_args = "--with-opt-dir=/usr/local:/opt/local:/sw:#{File.join(File.dirname(__FILE__), '../vendor', 'taglib')} "
+else
+  configure_args = "--with-opt-dir=/usr/local:/opt/local:/sw "
+end
+
 ENV['CONFIGURE_ARGS'] = configure_args + ENV.fetch('CONFIGURE_ARGS', "")
 
 require 'mkmf'

461a907c7ad1f3af6c2fe9110ba6da4c6427485a
Peter Roome <pete@wearepandr.com>
2012-12-05 21:08:06 +0000

    Incremend gem version build

--- ext/extconf_common.rb
+++ ext/extconf_common.rb
@@ -9,4 +9,32 @@ ENV['CONFIGURE_ARGS'] = configure_args + ENV.fetch('CONFIGURE_ARGS', "")
 
 require 'mkmf'
 
+if `uname -a`.strip[/Linux/i] && `uname -a`.strip[/x86_64/i]
+  def error msg
+    message msg + "\n"
+    abort
+  end
+
+  dir_config('tag')
+
+  # When compiling statically, -lstdc++ would make the resulting .so to
+  # have a dependency on an external libstdc++ instead of the static one.
+  unless $LDFLAGS.split(" ").include?("-static-libstdc++")
+    if not have_library('stdc++')
+      error "You must have libstdc++ installed."
+    end
+  end
+
+  if not have_library('tag')
+    error <<-DESC
+  You must have taglib installed in order to use taglib-ruby.
+
+  Debian/Ubuntu: sudo apt-get install libtag1-dev
+  Fedora/RHEL: sudo yum install taglib-devel
+  Brew: brew install taglib
+  MacPorts: sudo port install taglib
+  DESC
+  end
+end
+
 $CFLAGS << " -DSWIG_TYPE_TABLE=taglib"
